7663e990c30e04b72a8edf18db9bd60b6a9e0ac0
Audrey Cannesson <103568338+AudreyCa@users.noreply.github.com>
2023-03-07 15:56:07 +0100

    ajustement suite deploiement

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // API_URL: 'https://quickdocs-backend.onrender.com'
-  API_URL: 'http://127.0.0.1:3000'
+  API_URL: 'https://quickdocs-backend.onrender.com'
+  // API_URL: 'http://127.0.0.1:3000'
 };
